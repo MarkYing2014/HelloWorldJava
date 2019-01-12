ce3fdf9c7c634a23f2efa6bcd224d069cbb1ed17
yingmark2014 <MarkYing2014@gmail.com>
2019-01-12 11:07:16 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("HELLO WORLD AGAIN");
+        System.out.println("你好！楼应泽");
+    }
+}
